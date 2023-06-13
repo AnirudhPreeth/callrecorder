a9918706a01b15f2bc099c564db6d1ae18a601bc
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-06-13 13:40:31 +0530

    Updates.

--- android/app/CallRecorder.java
+++ android/app/CallRecorder.java
@@ -1,12 +1,38 @@
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import android.os.Bundle;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import java.io.File;
 import java.io.IOException;
+import com.example.myapp.CallRecorderMethodCallHandler
 
-public class CallRecorder {
+public class MainActivity extends FlutterActivity {
+    private static final String CALL_RECORDER_CHANNEL = "call_recorder_channel";
     private MediaRecorder recorder;
 
-    public void startRecording() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ...
+
+        MethodChannel methodChannel = new MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CALL_RECORDER_CHANNEL);
+        methodChannel.setMethodCallHandler((call, result) -> {
+            // Handle method invocations here
+            if (call.method.equals("startCallRecording")) {
+                startCallRecording();
+                result.success(null); // Return a success result if needed
+            } else if (call.method.equals("stopCallRecording")) {
+                stopCallRecording();
+                result.success(null); // Return a success result if needed
+            } else {
+                result.notImplemented(); // Handle unknown method invocations
+            }
+        });
+    }
+
+    private void startCallRecording() {
         recorder = new MediaRecorder();
 
         // Set the audio source
@@ -37,7 +63,7 @@ public class CallRecorder {
         recorder.start();
     }
 
-    public void stopRecording() {
+    private void stopCallRecording() {
         if (recorder != null) {
             // Stop recording
             recorder.stop();
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 import 'package:flutter/services.dart';
 import 'package:callrecorder/android/call_recorder_channel.dart';
-import 'package:callrecorder/recorder.dart';
 
 const number = '+918618764563';
 
@@ -16,22 +15,12 @@ void startCallRecording() async {
   }
 }
 
-void stopCallRecording() async {
-  try {
-    const platform = MethodChannel('call_recorder_channel');
-    await platform.invokeMethod('stopCallRecording');
-    print('Call recording stopped');
-  } catch (e) {
-    print('Failed to stop call recording: $e');
-  }
-}
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -41,15 +30,8 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final CallRecorder callRecorder = CallRecorder();
 
   @override
   Widget build(BuildContext context) {
@@ -58,104 +40,14 @@ class _HomePageState extends State<HomePage> {
         title: const Text("App"),
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                FlutterPhoneDirectCaller.callNumber(number);
-                CallRecorder.startRecording();
-              },
-              child: const Text("Call & Start Recording"),
-            ),
-            const SizedBox(width: 20), // Add spacing between buttons
-            ElevatedButton(
-              onPressed: () {
-                CallRecorder.stopRecording();
-              },
-              child: const Text("Stop Recording"),
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () async {
+            FlutterPhoneDirectCaller.callNumber(number);
+            startCallRecording();
+          },
+          child: const Text("Call & Start Recording"),
         ),
       ),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({Key? key}) : super(key: key);
-
-  @override
-  State<MainPage> createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Recording"),
-        centerTitle: true,
-      ),
-      backgroundColor: Colors.black87,
-      body: Column(
-        children: <Widget>[
-          const Text("Align Button to Bottom"),
-          Expanded(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: buildStart(),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildStart() {
-    const isRecording = false; // Replace with your recording logic
-    const icon = isRecording ? Icons.stop : Icons.mic;
-    const text = isRecording ? "Stop" : "Start";
-    const primary = isRecording ? Colors.red : Colors.white;
-    const onPrimary = isRecording ? Colors.white : Colors.black;
-
-    return ElevatedButton.icon(
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Colors.black, backgroundColor: Colors.red, minimumSize: const Size(175, 50),
-      ),
-      icon: const Icon(Icons.mic),
-      label: const Text(
-        'Start',
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => recorder()),
-        );
-        // Handle button press
-      },
-    );
-  }
-
-  recorder() {}
-}
-
-void demo() {
-  runApp(const MyAppy());
-}
-
-class MyAppy extends StatelessWidget {
-  const MyAppy({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MainPage(),
     );
   }
 }
