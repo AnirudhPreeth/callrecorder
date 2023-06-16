021cbbddfe1a93d48cf793fefebe27d11e376f54
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-06-16 11:37:48 +0530

    Updates.

--- lib/android/call_recorder_channel.dart
+++ lib/android/call_recorder_channel.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/services.dart';
 
-const MethodChannel _channel = MethodChannel('call_recorder_channel');
+const MethodChannel _callRecorderChannel = MethodChannel('call_recorder_channel');
 
 class CallRecorder {
   static Future<void> startRecording() async {
     try {
-      await _channel.invokeMethod('startCallRecording');
+      await _callRecorderChannel.invokeMethod('startCallRecording');
       print('Call recording started');
     } catch (e) {
       print('Failed to start call recording: $e');
@@ -14,7 +14,7 @@ class CallRecorder {
 
   static Future<void> stopRecording() async {
     try {
-      await _channel.invokeMethod('stopCallRecording');
+      await _callRecorderChannel.invokeMethod('stopCallRecording');
       print('Call recording stopped');
     } catch (e) {
       print('Failed to stop call recording: $e');
